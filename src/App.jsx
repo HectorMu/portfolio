a7e23376e9ab8a2148f55ea3f263ce87c31a3ff9
Héctor Manuel Muñoz Flores <skyspartanblack@gmail.com>
2022-02-10 00:59:12 -0600

    Added the hero demo for my portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Hero, Navbar, Layout } from "./components";
 import "./css/main.css";
+import "./css/utilities.css";
 
 const App = () => {
   return (
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,35 @@
 import React from "react";
 
 const Hero = () => {
-  return <div>Hero development</div>;
+  return (
+    <section className="hero bg-gray-light">
+      <div className="container">
+        <div className="hero-items py-1">
+          <div>
+            <img
+              src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+              alt="me"
+            />
+          </div>
+          <div className="about">
+            <h5>Who i am?</h5>
+            <p>
+              Hi, I'm Hector Muñoz a JavaScript fullstack enginner that loves to
+              learn new techonologies and take projects to the next level.
+            </p>
+            <div className="social-media">
+              <a href="#" className="btn-github">
+                <i class="fab fa-github fa-3x"></i>
+              </a>
+              <a href="#" className="btn-linkedin">
+                <i class="fab fa-linkedin fa-3x"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
