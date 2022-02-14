73709b87d0898e58c6fc28a614e6c9420ab35701
HectorMu <skyspartanblack@gmail.com>
2022-02-14 10:09:25 -0600

    Created sidebar with flexbox to smooth animation

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div>
       <Navbar />
-      <div className={`wrapper ${isActive ? `active` : ``}`}>
+      <div className={`wrapper`}>
         <Sidebar />
         <Layout>
           <Hero />
